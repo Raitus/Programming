393cbb537b55bcb6290fb0249d0fdab200cfeb17
Raitus <rd.raitus@gmail.com>
2021-07-29 13:26:48 +0300

    Initial commit

---
+++ Skillbox C++/Module 12/12.4.1/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+  std::cout << "--- Human ant hill ---\n" << std::endl;
+  std::string array[3];
+  int length = sizeof(array)/sizeof(array[0]);
+  for (int i = 0; i < length; ++i) {
+	std::cin>>array[i];
+  }
+  for (int i = 0, flatNumber; i < 3; ++i) {
+    do{
+	  std::cin>>flatNumber;
+	  if (flatNumber<0||flatNumber>length){
+		std::cout<<"Incorrect apartment number! Try again.\n";
+	  }
+    }while(flatNumber<=0||flatNumber>length);
+
+	std::cout<<array[flatNumber-1]<<std::endl;
+  }
+  return 0;
+}
---
+++ Skillbox C++/Module 12/12.4.2/cmake-build-debug/CMakeFiles/12_4_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "12_4_2.exe"
+  "12_4_2.exe.manifest"
+  "12_4_2.lib"
+  "12_4_2.pdb"
+  "CMakeFiles/12_4_2.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/12_4_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
