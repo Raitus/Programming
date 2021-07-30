d3b3559214b4b3a0e1a036787984d0a224d799f9
Raitus <rd.raitus@gmail.com>
2021-07-30 08:09:08 +0300

    Initial commit

---
+++ Skillbox C++/Module 12/12.4.3/cmake-build-debug/CMakeFiles/12_4_3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "12_4_3.exe"
+  "12_4_3.exe.manifest"
+  "12_4_3.lib"
+  "12_4_3.pdb"
+  "CMakeFiles/12_4_3.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/12_4_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
