6d4280207874e0cb08754c97d93427697368c2c0
Raitus <rd.raitus@gmail.com>
2021-08-12 11:03:31 +0300

    Resolved using one cycle

--- Skillbox_C++/Module_14/14.6.3/main.cpp
+++ Skillbox_C++/Module_14/14.6.3/main.cpp
@@ -4,20 +4,19 @@ int main() {
   std::cout << "--- Snake way ---\n" << std::endl;
   const int x{5}, y{5};
   int array[x][y];
-  for (int i = 0, number = 0; i < y; ++i) {
-	for (int j = 0; j < x; ++j, ++number) {
-	  array[i][j] = number;
-	  std::cout<<array[i][j]<<"\t";
-	}
-	std::cout<<std::endl;
-	++i;
-	for (int j = x - 1; i < y && j >= 0; --j, ++number) {
-	  array[i][j] = number;
-	}
-	for (int j = 0; i < y && j < x; ++j) {
-	  std::cout<<array[i][j]<<"\t";
-	}
-	std::cout<<std::endl;
+
+  for (int j=0, number = 0; number < y*x; ++number, ++j) {
+    array[j%x][j/x]=number+(((x-1)-2*(j%x))*(j/x%2));
+    std::cout<<array[j%x][j/x]<<' ';
+
+    if (j%x==4)std::cout<<std::endl; //"if" is used to transfer to a new line, we don`t need to use the second cycle
+
+	/*for (int i = 4; i >=-4; i-=2, ++number, ++j) {
+	  array[j%x][j/x]=number+(i*(j/x%2));
+	  std::cout<<array[j%x][j/x]<<' ';
+	}*/
   }
+
+
   return 0;
 }
