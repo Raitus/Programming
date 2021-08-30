f63c3975efc7c6ae330203267ee770b48a883b93
Raitus <rd.raitus@gmail.com>
2021-08-30 11:35:05 +0300

    14.6.7 corrected mistakes after revision

--- Skillbox_C++/Module_14/14.6.7/main.cpp
+++ Skillbox_C++/Module_14/14.6.7/main.cpp
@@ -16,41 +16,39 @@ void Popping(std::array<std::array<bool, 12>, 12> &pupyrka, int &count, int &x1,
   }
 }
 
+bool CoordinatesCorrectness(int &x, int &y){
+  if (x<1||x>12||y<1||y>12) {
+    std::cout<<"You made mistake. Try again!\n";
+    return false;
+  }
+  return true;
+}
+
 void PopRegion(std::array<std::array<bool, 12>, 12> &pupyrka, int &count) {
   std::cout << "Choose region to make a Pop!\n";
 int x1, x2, y1, y2;
-bool check;
   do {
-    check=false;
     std::cout<<"Input first coordinate in format x >> y: ";
     std::cin>>x1>>y1;
-    if (x1<1||x1>12||y1<1||y1>12) {
-      std::cout<<"You made mistake. Try again!\n";
-      check=true;
-    }
-  }while(check);
+  }while(!CoordinatesCorrectness(x1, y1));
   do {
-    check=false;
     std::cout<<"Input second coordinate in format x >> y: ";
     std::cin>>x2>>y2;
-    if (x2<1||x2>12||y2<1||y2>12) {
-      std::cout<<"You made mistake. Try again!\n";
-      check=true;
-    }
-  }while(check);
+  }while(!CoordinatesCorrectness(x2, y2));
+
   if (y1<y2){
-    if (x1<x2){
-	  Popping(pupyrka, count, x1, y1, x2, y2);
-    }else{
-      Popping(pupyrka, count, x2, y1, x1, y2);
+    if (x1>=x2){
+      std::swap(x1,x2);
     }
   }else{
     if (x1<x2){
-      Popping(pupyrka, count, x1, y2, x2, y1);
+      std::swap(y1,y2);
     }else{
-      Popping(pupyrka, count, x2, y2, x1, y1);
+      std::swap(x1,x2);
+      std::swap(y1,y2);
     }
   }
+  Popping(pupyrka, count, x1, y1, x2, y2);
 }
 
 void ArrayPrint(std::array<std::array<bool, 12>, 12> &pupyrka) {
@@ -79,6 +77,5 @@ int main() {
     ArrayPrint(pupyrka);
     PopRegion(pupyrka, count);
   }while(count!=0);
-
   return 0;
 }
