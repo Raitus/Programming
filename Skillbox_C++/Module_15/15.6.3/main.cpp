dd137787e0772d4b19e2b6d16aa851634e194cc9
Raitus <rd.raitus@gmail.com>
2021-09-20 12:16:40 +0300

    15.6.3 completed exercise

--- Skillbox_C++/Module_15/15.6.3/main.cpp
+++ Skillbox_C++/Module_15/15.6.3/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  std::cout << "--- M15 Exercise 3 ---" << std::endl;
+  std::vector<int> list;
+  int num;
+  do {
+    std::sort(list.begin(), list.end());
+    if (list.size() > 4) {
+      std::cout << list[4] << std::endl;
+    } else {
+      std::cout << "This array doesn't have even 5 elements." << std::endl;
+    }
+    std::cout << "Input list of numbers: ";
+    bool check;
+    do {
+      check = false;
+      std::cin >> num;
+      if (num != -1 && num != -2) {
+        list.push_back(num);
+        check = true;
+      }
+    } while (check);
+  } while (num != -2);
   return 0;
 }
