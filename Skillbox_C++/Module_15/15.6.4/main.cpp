69b7f612ff8fa08645e9b56d42ae6127219f3c7f
Raitus <rd.raitus@gmail.com>
2021-09-20 12:49:01 +0300

    15.6.4 Completed exercise with optimisations

--- Skillbox_C++/Module_15/15.6.4/main.cpp
+++ Skillbox_C++/Module_15/15.6.4/main.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
+#include <array>
+
+void PrintArray(std::array<int,6> &array) {
+  std::cout << "Sorted massive of numbers: ";
+  for (int i : array) {
+    std::cout << i << " ";
+  }
+}
+
+void ShakerSort(std::array<int,6> &array) {
+  bool change{true};
+  for (int i = 0, rCount = 0, lCount = 0; i < array.size() && change; ++i) {
+    change = false;
+    for (int j = 1 + lCount; j < array.size() - rCount; ++j) {
+      if (std::abs(array[j - 1]) > std::abs(array[j])) {
+        change = true;
+        std::swap(array[j], array[j - 1]);
+        if (j==array.size() - rCount - 1) {
+          rCount++;
+        }
+      }
+    }
+    if (change) {
+      change = false;
+      for (int j = array.size() - rCount - 1; j >= 1 + lCount; --j) {
+        if (std::abs(array[j - 1]) > std::abs(array[j])) {
+          change = true;
+          std::swap(array[j], array[j - 1]);
+          if (j==0 + lCount) {
+            lCount++;
+          }
+        }
+      }
+    }
+  }
+  PrintArray(array);
+}
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  std::cout << "--- M15 Exercise 4 ---" << std::endl;
+  std::array<int,6> array{-100, -50, -5, 1, 10, 15};
+  ShakerSort(array);
   return 0;
 }
