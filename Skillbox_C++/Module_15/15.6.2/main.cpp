b0ffed430845f983817e5baa2e74d23a43521c94
Raitus <rd.raitus@gmail.com>
2021-09-20 11:45:29 +0300

    15.6.2 completed exercise

--- Skillbox_C++/Module_15/15.6.2/main.cpp
+++ Skillbox_C++/Module_15/15.6.2/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <array>
+#include <algorithm>
+
+bool FindElements(std::array<int, 5> &list, int resNum, int &firstNum, int &secondNum) {
+  for (int i = 0; i < list.size() - 1 && list[i] < resNum; ++i) {
+    for (int j = i + 1; j < list.size() && list[j] < resNum; ++j) {
+      if (list[i] + list[j] == resNum) {
+        firstNum = list[i];
+        secondNum = list[j];
+        return true;
+      }
+    }
+  }
+  return false;
+}
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  std::cout << "--- M15 Exercise 2 ---" << std::endl;
+  std::array<int, 5> list{2, 1, 11, 7, 15};
+  int resNum, firstNum, secondNum;
+  std::cout << "Input result number: ";
+  std::cin >> resNum;
+  std::sort(list.begin(), list.end());
+  if (FindElements(list, resNum, firstNum, secondNum)) {
+    std::cout << "The sum of numbers " << firstNum << " and " << secondNum << " give us the result " << resNum << "."
+              << std::endl;
+  } else {
+    std::cout << "This array doesn't have numbers to give us result " << resNum << "." << std::endl;
+  }
   return 0;
 }
