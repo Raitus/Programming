a7ddacd35a75b917567c426b91e9414304fb46d9
Raitus <rd.raitus@gmail.com>
2021-09-24 14:23:51 +0300

    15.6.1 corrected exercise after revision

--- Skillbox_C++/Module_15/15.6.1/main.cpp
+++ Skillbox_C++/Module_15/15.6.1/main.cpp
@@ -3,18 +3,40 @@
 
 int main() {
   std::cout << "--- Exercise 1 ---" << std::endl;
-  std::array<int, 9> list = {-2, 1, 3, -4, -1, 2, 1, 5, 4};
-  int maxSum{list[0] + list[1] + list[2] + list[3]}, firstElement{0};
-  for (int i = 1, sum{maxSum}; i < list.size() - 3; ++i) {
-    sum += list[i + 3] - list[i - 1];
-    if (sum > maxSum) {
-      maxSum = sum;
-      firstElement = i;
+  std::array<int, 9> list = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+  int firstElement, finalElement, maxSum{0};
+  for (int i = 0; i < list.size(); ++i) {
+    int sum, firstIndex, secondIndex;
+    sum = 0;
+    if (list[i] > 0) {
+      firstIndex = i;
+      secondIndex = i;
+      sum += list[i];
+      for (int j = i + 1, minusCount{0}; i < list.size() - 1 && j < list.size(); ++j) {
+        if (list[j] < 0) {
+          minusCount += list[j];
+        } else if (std::abs(minusCount) < list[j]) {
+          secondIndex = j;
+          sum += list[j] + minusCount;
+          minusCount = 0;
+        }
+      }
+      for (int j = i - 1, minusCount{0}; i > 0 && j > 0; --j) {
+        if (list[j] < 0) {
+          minusCount += list[j];
+        } else if (std::abs(minusCount) < list[j]) {
+          secondIndex = j;
+          sum += list[j] + minusCount;
+          minusCount = 0;
+        }
+      }
+      if (maxSum < sum) {
+        maxSum = sum;
+        firstElement = firstIndex;
+        finalElement = secondIndex;
+      }
     }
   }
-  std::cout << "Max sum of elements located between " << firstElement << " and " << firstElement + 3 << " indexes': {"
-            << list[firstElement] << "," << list[firstElement + 1] << "," << list[firstElement + 2] << ","
-            << list[firstElement + 3] << "}";
-
+  std::cout << "Max sum of elements located between " << firstElement << " and " << finalElement << ".";
   return 0;
 }
