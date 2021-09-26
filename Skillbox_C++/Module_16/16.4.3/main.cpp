3e344c009c4cc35ea39b80d7bb8dedc92604c119
Raitus <rd.raitus@gmail.com>
2021-09-26 21:20:19 +0300

    16.4.3 Completed exercise

--- Skillbox_C++/Module_16/16.4.3/main.cpp
+++ Skillbox_C++/Module_16/16.4.3/main.cpp
@@ -1,6 +1,68 @@
 #include <iostream>
+#include <string>
+
+bool StringCheck(std::string &text) {
+  for (char i: text) {
+    if (!((i >= '/' && i <= '9') || i == '*' || i == '-' || i == '+' || i == '.')) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void StringInput(std::string &inputField) {
+  bool check{false};
+  do {
+    std::cout << "Input string: ";
+    std::cin >> inputField;
+    if (StringCheck(inputField)) {
+      check = true;
+    } else {
+      std::cout << "Wrong input! Try again." << std::endl;
+    }
+  } while (!check);
+}
+
+void Calculation(double firstNumber, char arithmeticSign, double secondNumber) {
+  std::cout << "Result is ";
+  if (arithmeticSign == '+') {
+    std::cout << firstNumber + secondNumber;
+  } else if (arithmeticSign == '-') {
+    std::cout << firstNumber - secondNumber;
+  } else if (arithmeticSign == '*') {
+    std::cout << firstNumber*secondNumber;
+  } else {
+    if (secondNumber != 0) {
+      std::cout << firstNumber/secondNumber;
+    } else {
+      std::cout << firstNumber;
+    }
+  }
+}
+
+void StringDefragmentation(std::string &inputField) {
+  std::string firstNumber, secondNumber;
+  char arithmeticSign;
+  bool firstPartCheck{false};
+  for (char i : inputField) {
+    if (!firstPartCheck) {
+      if (!(i == '*' || i == '-' || i == '+' || i == '/')) {
+        firstNumber += i;
+      } else {
+        arithmeticSign = i;
+        firstPartCheck = true;
+      }
+    } else {
+      secondNumber += i;
+    }
+  }
+  Calculation(std::stod(firstNumber), arithmeticSign, std::stod(secondNumber));
+}
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  std::cout << "--- M16 Calculator ---" << std::endl;
+  std::string inputField;
+  StringInput(inputField);
+  StringDefragmentation(inputField);
   return 0;
 }
