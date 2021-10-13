15181b0233a0748058c9e8f94fd1893c1b0e570c
Raitus <rd.raitus@gmail.com>
2021-10-13 10:38:13 +0300

    17.4.1 completed exercise

--- Skillbox_C++/Module_17/17.4.1/main.cpp
+++ Skillbox_C++/Module_17/17.4.1/main.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 
-void swap(int *variable1, int *variable2){
-  int variableTemp=*variable1;
-  *variable1=*variable2;
-  *variable2=variableTemp;
+void swap(int *numA, int *numB){
+  int tempNum = *numA;
+  *numA=*numB;
+  *numB=tempNum;
 }
 
 int main() {
-  std::cout << "--- M17 Exercise 1 ---" << std::endl;
+  std::cout << "--- M17 Exercise 1" << std::endl;
   int a = 10;
   int b = 20;
   swap(&a, &b);
-  std::cout << a << " " << b; // 20 10
+  std::cout << a << " " << b;
   return 0;
 }
