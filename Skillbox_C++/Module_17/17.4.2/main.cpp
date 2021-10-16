812a4a316e03ec24ff896744a49ab2a8a04df9ed
Raitus <rd.raitus@gmail.com>
2021-10-16 16:49:55 +0300

    17.4.2 added global variable

--- Skillbox_C++/Module_17/17.4.2/main.cpp
+++ Skillbox_C++/Module_17/17.4.2/main.cpp
@@ -1,26 +1,28 @@
 #include <iostream>
 
-void ReversCopy(int number[10]){
-  for (int i = 0, temp; i < 5; ++i) {
-    temp=number[0+i];
-    number[0+i]=number[9-i];
-    number[9-i]=temp;
+const int arrLength{10};
+
+void ReversCopy(int number[arrLength]) {
+  for (int i = 0, temp; i < arrLength/2; ++i) {
+    temp = number[0 + i];
+    number[0 + i] = number[arrLength - 1 - i];
+    number[arrLength - 1 - i] = temp;
   }
 }
 
-void ArrayOutput(int numbers[10]){
-  std::cout<<"Revers array: ";
-  for (int i = 0; i < 10; ++i) {
-    std::cout<<numbers[i]<<" ";
+void ArrayOutput(int numbers[arrLength]) {
+  std::cout << "Revers array: ";
+  for (int i = 0; i < arrLength; ++i) {
+    std::cout << numbers[i] << " ";
   }
 }
 
 int main() {
   std::cout << "--- M17 exercise 2" << std::endl;
-  int numbers[10];
-  std::cout<<"Input 10 numbers: ";
-  for (int & number : numbers) {
-    std::cin>>number;
+  int numbers[arrLength];
+  std::cout << "Input 10 numbers: ";
+  for (int &number: numbers) {
+    std::cin >> number;
   }
   ReversCopy(numbers);
   ArrayOutput(numbers);
