b457792c22191657a13dff7aefcfd5899e3b624f
raitus <rd.raitus@gmail.com>
2022-02-08 11:40:32 +0200

    2.3.1 Completed exercise

--- Coursera_C++/Week 2/2.3.1_Anagrams/main.cpp
+++ Coursera_C++/Week 2/2.3.1_Anagrams/main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <map>
+#include <string>
+
+void CharAnalyser(std::map<char, int>& vocabulary, const std::string& word){
+    for (char i : word) {
+        vocabulary[i]++;
+    }
+}
+
+bool BuildCharCounters(std::map<char, int>& firstMap, std::map<char, int>& secondMap,
+                       const std::string& firstWord, const std::string& secondWord){
+    CharAnalyser(firstMap, firstWord);
+    CharAnalyser(secondMap, secondWord);
+    if (firstMap==secondMap){
+        return true;
+    }else{
+        return false;
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::map<char, int> firstWordCharCount, secondWordCharCount;
+    int wordsNumber;
+    std::cin>>wordsNumber;
+    for (int i = 0; i < wordsNumber; ++i) {
+        std::string firstWord, secondWord;
+        std::cin>>firstWord>>secondWord;
+        if (BuildCharCounters(firstWordCharCount, secondWordCharCount, firstWord, secondWord)){
+            std::cout<<"true"<<std::endl;
+        }else{
+            std::cout<<"false"<<std::endl;
+        }
+        firstWordCharCount.clear();
+        secondWordCharCount.clear();
+    }
     return 0;
 }
