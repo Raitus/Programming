f964f8117757f38ae95cc485f230ebfc976ee27f
raitus <rd.raitus@gmail.com>
2022-02-22 12:37:48 +0200

    2.3.4 Completed exercise

--- Coursera_C++/Week 2/2.3.4_Bus_Stops_2/main.cpp
+++ Coursera_C++/Week 2/2.3.4_Bus_Stops_2/main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
 #include <map>
+#include <vector>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::map<int, std::vector<std::string>> busList;
+    int commandCount;
+    std::cin >> commandCount;
+    for (int i = 0; i < commandCount; ++i) {
+        int stationCount;
+        std::cin >> stationCount;
+        std::vector<std::string> station(stationCount);
+        for (int j = 0; j < stationCount; ++j) {
+            std::cin >> station[j];
+        }
+        if (busList.empty()) {
+            std::cout << "New bus 1" << std::endl;
+            busList[1] = station;
+        } else {
+            for (auto &line: busList) {
+                if (stationCount == line.second.size() && line.second == station) {
+                        std::cout << "Already exists for " << line.first << std::endl;
+                        break;
+                } else if (line.first == busList.end()->first) {
+                    int size{static_cast<int>(busList.size()) + 1};
+                    std::cout << "New bus " << size << std::endl;
+                    busList[size] = station;
+                    break;
+                }
+            }
+        }
+    }
     return 0;
 }
