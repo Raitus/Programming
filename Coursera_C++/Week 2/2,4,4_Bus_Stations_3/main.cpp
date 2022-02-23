c08fef8eca3a6ad95b5fc7407b54fcae4aea808e
raitus <rd.raitus@gmail.com>
2022-02-23 17:12:13 +0200

    2,4,4 Completed exercise

--- Coursera_C++/Week 2/2,4,4_Bus_Stations_3/main.cpp
+++ Coursera_C++/Week 2/2,4,4_Bus_Stations_3/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <set>
+#include <string>
+#include <vector>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int operationCount;
+    std::cin >> operationCount;
+    std::vector<std::set<std::string>> busLines;
+    for (int i = 0; i < operationCount; ++i) {
+        int busStations;
+        std::cin >> busStations;
+        std::set<std::string> stations;
+        for (int j = 0; j < busStations; ++j) {
+            std::string word;
+            std::cin >> word;
+            stations.insert(word);
+        }
+        bool check{false};
+        for (int j{0}; j < busLines.size(); j++) {
+            if (busLines[j] == stations) {
+                std::cout << "Already exists for " << j+1 << std::endl;
+                check = true;
+                break;
+            }
+        }
+        if (!check) {
+            std::cout << "New bus " << busLines.size() + 1 << std::endl;
+            busLines.push_back(stations);
+        }
+    }
 }
